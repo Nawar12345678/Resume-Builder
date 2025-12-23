dac2503951f1987f13bdf7b1ebc427724dded6ad
Nawar <nawaralissa23@gmail.com>
2025-12-23 17:38:33 +0300

    second commit

---
+++ client/src/components/ExperienceForm.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const ExperienceForm = ({ data, onChange }) => {
+    return (
+        <div className='space-y-6'>
+
+        </div>
+    )
+}
+
+export default ExperienceForm
